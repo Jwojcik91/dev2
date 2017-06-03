20433d521025ceb18ce91e491ff69bf8d833150f
Jacek <jacek.wojcik91@gmail.com>
2017-06-03 16:22:57 +0200

    fix path in travis script

--- scripts/dotnet-restore.sh
+++ scripts/dotnet-restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source="--source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/devw/api/v3/index.json --no-cache"
-cd ../Warsztaty
+cd ..
 projects=(Warsztaty warsztaty.messages Warsztaty.Service)
 for project in ${projects[*]}
 do
